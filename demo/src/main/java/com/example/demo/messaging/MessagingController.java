d612179e6e0d176e89a195391aa4c579bceb2f93
user-pak <bagh84733@gmail.com>
2022-10-25 18:42:59 +0900

    add username with echo message

--- demo/src/main/java/com/example/demo/messaging/MessagingController.java
+++ demo/src/main/java/com/example/demo/messaging/MessagingController.java
@@ -1,7 +1,10 @@
 package com.example.demo.messaging;
 
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,9 +12,11 @@ public class MessagingController {
 	
 	@MessageMapping("/hello")
 	@SendTo("/topic/messaging")
-	public MessageContent helloGreeting(MessageContent content) throws InterruptedException {
+	public MessageContent helloGreeting(MessageContent content, MessageHeaders messageHeaders) throws InterruptedException {
 		Thread.sleep(1000);
-		return new MessageContent("echo:" + content.getContent());
+		StompHeaderAccessor stompHeaderAccessor = MessageHeaderAccessor.getAccessor(messageHeaders, StompHeaderAccessor.class);
+		String echo = stompHeaderAccessor.getUser().getName();
+		return new MessageContent(echo + " : " + content.getContent());
 	}
 
 }
